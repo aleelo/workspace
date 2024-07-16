18c739e60e234f288b5dd0d4fe610dbbfdea9c14
fahadmumin <fahadmumin8@gmail.com>
2024-07-16 08:14:30 -0700

    16-july

--- app/Controllers/Leaves.php
+++ app/Controllers/Leaves.php
@@ -124,7 +124,7 @@ class Leaves extends Security_Controller {
                     $leave_email_data = [
                         'LEAVE_ID'=>$save_id,
                         'EMPLOYEE_NAME'=>$user_info->first_name.' '.$user_info->last_name,
-                        // 'LEAVE_TITLE' => $leave_info->title,
+                        'LEAVE_TITLE' => $leave_info->title,
                         // 'LEAVE_REASON' => $leave_info->reason,
                         // 'LEAVE_DATE' => $duration == 1 ? $leave_data['start_date']: $leave_data['start_date'] .' - '.$leave_data['end_date'],
                         // 'TOTAL_DAYS'=>(int)$leave_info->total_days,
