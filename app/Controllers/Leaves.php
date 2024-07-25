5c55db8d928524dbb3c555e43f0794a5d7b2c775
fahadmumin <fahadmumin8@gmail.com>
2024-07-25 03:52:24 -0700

    25-july

--- app/Controllers/Leaves.php
+++ app/Controllers/Leaves.php
@@ -1184,7 +1184,7 @@ class Leaves extends Security_Controller {
     //return required style/format for a application
     private function _prepare_leave_info($data) {
         $image_url = get_avatar($data?->applicant_avatar);
-        $data->applicant_meta = "<span class='avatar avatar-xs mr10'><img src='$image_url' alt=''></span>" . $data?->applicant_name;
+        $data?->applicant_meta = "<span class='avatar avatar-xs mr10'><img src='$image_url' alt=''></span>" . $data?->applicant_name;
         $style = '';
 
         if (isset($data->status)) {
