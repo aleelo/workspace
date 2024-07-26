105e675e88a2c71f9029a3ab002d20a0e9b970e1
fahadmumin <fahadmumin8@gmail.com>
2024-07-27 01:27:05 +0300

    22222

--- app/Views/leaves/application_details.php
+++ app/Views/leaves/application_details.php
@@ -108,7 +108,7 @@
 
         <?php if ($role !== 'Director' && $role !== 'Employee' ) { ?>
 
-            <button data-status="verified" type="submit" class="btn btn-success btn-sm update-leave-status"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('verify_sign'); ?></button>
+            <button data-status="pending" type="submit" class="btn btn-success btn-sm update-leave-status"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('verify_sign'); ?></button>
             <button data-status="approved" type="submit" class="btn btn-success btn-sm update-leave-status"><span data-feather="check-circle" class="icon-16"></span> <?php echo app_lang('approve_sign'); ?></button>
             
         <?php }else if ($role == 'Director' ) { ?>
