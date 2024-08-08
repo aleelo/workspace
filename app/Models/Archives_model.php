d5ee50a441933209603d1ee72e7975880018c987
fahadmumin <fahadmumin8@gmail.com>
2024-08-08 07:18:57 -0700

    08-aug

--- app/Models/Archives_model.php
+++ app/Models/Archives_model.php
@@ -40,7 +40,7 @@ class General_files_model extends Crud_model {
         $sql = "SELECT $archive_files_table.*, d.nameSo as department, CONCAT($users_table.first_name, ' ', $users_table.last_name) AS uploaded_by_user_name, $users_table.image AS uploaded_by_user_image, $users_table.user_type AS uploaded_by_user_type
         FROM $archive_files_table
         LEFT JOIN $users_table ON $users_table.id= $archive_files_table.uploaded_by
-        LEFT JOIN departments d ON $archive_files_table.department_id = d.id
+        LEFT JOIN rise_departments d ON $archive_files_table.department_id = d.id
         WHERE $archive_files_table.deleted=0 $where";
         return $this->db->query($sql);
     }
