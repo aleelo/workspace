cdccfa1d830d6d8e84ef00063e98e684e4812567
fahadmumin <fahadmumin8@gmail.com>
2024-10-01 03:52:10 -0700

    leave

--- app/Views/leaves/all_applications.php
+++ app/Views/leaves/all_applications.php
@@ -8,17 +8,18 @@
         $("#all-application-table").appTable({
             source: '<?php echo_uri("leaves/all_application_list_data") ?>',
             dateRangeType: "monthly",
+            rangeDatepicker: [{startDate: {name: "start_date", value: moment().format("YYYY-MM-DD")}, endDate: {name: "end_date", value: moment().format("YYYY-MM-DD")}}],
             columns: [
                 {title: '<?php echo 'ID' ?>', "class": "w10p"},
                 {title: '<?php echo app_lang("applicant") ?>', "class": "w20p"},
                 {title: '<?php echo app_lang("leave_type") ?>', "class": "w15p"},
                 {title: '<?php echo app_lang("date") ?>', "class": "w20p"},
-                {title: '<?php echo app_lang("duration") ?>', "class": "w20p"},
-                {title: '<?php echo app_lang("unit_name") ?>', "class": "w20p"},
+                // {title: '<?php //echo app_lang("duration") ?>', "class": "w20p"},
+                // {title: '<?php //echo app_lang("unit_name") ?>', "class": "w20p"},
                 {title: '<?php echo app_lang("section_name") ?>', "class": "w20p"},
                 {title: '<?php echo app_lang("dp_name") ?>', "class": "w20p"},
                 {title: '<?php echo app_lang("status") ?>', "class": "w100"},
-                {title: '<i data-feather="menu" class="icon-16"></i>', "class": "text-center option w10p"}
+                {title: '<i data-feather="menu" class="icon-16"></i>', "class": "text-center option w1000p"}
             ],
             printColumns: [0, 1, 2, 3, 4],
             xlsColumns: [0, 1, 2, 3, 4]
