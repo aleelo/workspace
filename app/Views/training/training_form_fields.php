c1c303fe72b474a46eaa96e89d6adbac0c584259
fahadmumin <fahadmumin8@gmail.com>
2024-10-08 16:45:31 -0700

    tri

--- app/Views/training/training_form_fields.php
+++ app/Views/training/training_form_fields.php
@@ -527,7 +527,7 @@ function resetOtherDropdownsFunder(excludeSection) {
 
         // Show the appropriate section(s) based on the selected meeting_with value and reset others
         switch (funder) {
-            case '4':
+            case '2':
                 $('#budget_section').show();
                 resetOtherDropdownsFunder('#budget_section');
                 break;
