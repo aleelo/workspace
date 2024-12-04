47e48e649048a7b06381844720938a4d29e35e52
fahadmumin <fahadmumin8@gmail.com>
2024-12-04 01:42:40 -0800

    update

--- app/Controllers/Expenses.php
+++ app/Controllers/Expenses.php
@@ -136,6 +136,8 @@ class Expenses extends Security_Controller {
         $repeat_every = $this->request->getPost('repeat_every');
         $repeat_type = $this->request->getPost('repeat_type');
         $no_of_cycles = $this->request->getPost('no_of_cycles');
+        $status = $this->request->getPost('status');
+
 
         $data = array(
             "expense_date" => $expense_date,
@@ -147,7 +149,7 @@ class Expenses extends Security_Controller {
             "client_id" => $this->request->getPost('expense_client_id') ? $this->request->getPost('expense_client_id') : 0,
             "project_id" => $this->request->getPost('expense_project_id'),
             "user_id" => $this->request->getPost('expense_user_id'),
-            "status" => $this->request->getPost('status'),
+            "status" => $status ? $status : "unpaid",
             "tax_id" => $this->request->getPost('tax_id') ? $this->request->getPost('tax_id') : 0,
             "tax_id2" => $this->request->getPost('tax_id2') ? $this->request->getPost('tax_id2') : 0,
             "recurring" => $recurring,
